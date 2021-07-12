f02831f3a0a6db1120ce74c7295d84dca2c3bf87
SadBarin <sadbarin@yandex.com>
2021-07-12 23:51:36 +0300

    Add AppObject

---
+++ src/components/AppObject.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="object">
+    <img class="object-image" src="../assets/images/furnace.png" alt="furnace">
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'AppObject'
+}
+</script>
+
+<style scoped>
+  .object {
+    border:        2px solid var(--border);
+    border-radius: var(--borderRadius);
+
+    width:  2.5rem;
+    height: 2.5rem;
+
+    overflow: hidden;
+  }
+
+  .object-image {
+    width:  100%;
+    height: 100%;
+  }
+</style>
--- src/components/AppObjectLibrary.vue
+++ src/components/AppObjectLibrary.vue
@@ -1,26 +1,43 @@
 <template>
   <div class="object-library">
+    <div class="object-library-content">
+      <AppObject v-for="element in elements" :key="element"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import AppObject from '@/components/AppObject.vue'
+
 export default {
-  name: 'AppObjectLibrary'
+  name: 'AppObjectLibrary',
+  components: { AppObject },
+  data () {
+    return { elements: 200 }
+  }
 }
 </script>
 
 <style scoped>
   .object-library {
-    border:        2px solid var(--border);
-    border-radius: var(--borderRadius);
-
     width:   100%;
     height:  100%;
     padding: var(--padding);
 
-    display:        grid;
-    gap:            var(--padding);
-    place-content:  start;
-    grid-auto-flow: column;
+    overflow: auto;
+  }
+
+  .object-library-content {
+    width:         100%;
+    height:        100%;
+    padding-right: var(--padding);
+
+    display:               grid;
+    grid-template-columns: repeat(auto-fill, 2.5rem);
+    grid-template-rows:    repeat(auto-fill, 2.5rem);
+    grid-gap:              var(--padding);
+    place-content:         space-between;
+
+    overflow: auto;
   }
 </style>
