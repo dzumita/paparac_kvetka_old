166cd051dfa0239fd6bb592ba56cf1d3cb2ced6b
paparac <sadbarin@yandex.com>
2021-07-17 20:54:51 +0300

    Added AppButton

---
+++ src/components/AppButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="button">{{text}}</button>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'AppButton',
+  props: {
+    text: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .button {
+    background: transparent;
+    border: 2px solid var(--activeColor);
+    border-radius: var(--borderRadius);
+
+    font-family: var(--mainFont);
+    color: var(--activeColor);
+
+    width: 100%;
+    height: 1.5rem;
+
+    cursor: pointer;
+  }
+
+  .button:active {
+    transform: scale(0.90);
+  }
+</style>
